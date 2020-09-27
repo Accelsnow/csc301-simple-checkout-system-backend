bf15c218dbd4aec13999d48e21a71dd914203c54
Junan Zhao <acceleratorsnow@gmail.com>
2020-09-27 13:30:44 -0400

    added AWS RDS database envrion

--- config.py
+++ config.py
@@ -4,11 +4,15 @@ from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 load_dotenv(os.path.join(basedir, '.env'))
 
+if os.environ['RDS_HOSTNAME']:
+    db_uri = "mysql://{}:{}@{}/{}".format(os.environ['RDS_USERNAME'], os.environ['RDS_PASSWORD'],
+                                          os.environ['RDS_HOSTNAME'], os.environ['RDS_DBNAME'])
+else:
+    db_uri = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "*(*@!^sdafl318"
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = db_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
