372b558465c02aa64936c80db0cb9ba382cb5f18
Junan Zhao <acceleratorsnow@gmail.com>
2020-09-27 13:30:06 -0400

    Revert "Tweaked application.py for server compatibility (added db existence check)"
    This reverts commit 762dd3813d544e0c6bb07c4e86270995c7480bde.
    
    Revert wrong application tweak

--- application.py
+++ application.py
@@ -4,11 +4,9 @@ from app.models import Customer, Manager, Item, Receipt, Checkout
 application = app
 
 if __name__ == '__main__':
-    import os
-
-    basedir = os.path.abspath(os.path.dirname(__file__))
-    if not os.path.exists(basedir + '/app.db'):
-        db.create_all()
+    db.create_all()
+    if not Manager.query.first() and not Checkout.query.first() and not Item.query.first():
+        print("create")
         customer1 = Customer(id=1, name="John Customer Doe")
         customer2 = Customer(id=2, name="Jane Customer Doe")
         manager = Manager(id=1, username="manager")
