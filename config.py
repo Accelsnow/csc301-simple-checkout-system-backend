8f6a0cd54310e3d2e22af199c3d0a0929f2fde85
Junan Zhao <acceleratorsnow@gmail.com>
2020-10-02 01:14:28 -0400

    Added SAMESITE = strict to flask cookie setting.

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
 if 'RDS_HOSTNAME' in os.environ:
     db_uri = "mysql+pymysql://{}:{}@{}/{}".format(os.environ['RDS_USERNAME'], os.environ['RDS_PASSWORD'],
                                                   os.environ['RDS_HOSTNAME'], os.environ['RDS_DB_NAME'])
@@ -14,6 +13,5 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or "9812(*&!@OIFdafdsafaljk"
     SQLALCHEMY_DATABASE_URI = db_uri
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SESSION_COOKIE_SECURE=True
-    SESSION_COOKIE_HTTPONLY=True
-    SESSION_COOKIE_SAMESITE="lax"
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = "strict"
